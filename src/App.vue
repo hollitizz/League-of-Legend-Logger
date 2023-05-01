25317baa5ef46c683e29a9a3db8ee144342c8979
Hollitizz <valentin.caure@epitech.eu>
2023-05-01 18:17:20 +0200

    [ADD] vue working fs command

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <p>test</p>
+</template>
+<script setup lang="ts"></script>
+<style>
+.flex-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+
+.logo.electron:hover {
+    filter: drop-shadow(0 0 2em #9feaf9);
+}
+
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
